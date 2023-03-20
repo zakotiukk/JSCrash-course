476d9b67edd69f1322ff69e8b669dd4fcf76e0d6
zakotiukk <zakotiukk@gmail.com>
2023-03-20 15:00:15 +0200

    code is modified

--- 01/main.js
+++ 01/main.js
@@ -10,7 +10,8 @@ console.log(`Area of circle with radius ${radius} is ${area}`);
 //Task 3
 const distance = prompt('What is the distance in km between two sities?');
 const time_having = prompt('What is the amount of time in hours you have to reach your destination?');
-console.log(`Your needed speed to arrive on time: ${distance/time_having} km/hour`);
+speed=distance/time_having;
+console.log(`Your needed speed to arrive on time: ${speed.toFixed(0)} km/hour`);
 
 //Task 4
 let dollars=prompt('How much dollars do you want to convert?');
@@ -19,7 +20,5 @@ console.log(`Amount of euro: ${dollars*rate}`);
 
 //Task 5
 const capacity =prompt("Enter the capacity of a flash drive in GB");
-convert =capacity *1024;
-res= convert/820;
-//console.log(res.toFixed(0));
-console.log(`The number of files with sixe 820 MB which can be saved on the flash drive: ${convert/820}`)
+let convert =capacity *1024;
+console.log(`The number of files with sixe 820 MB which can be saved on the flash drive: ${parseInt(convert/820)}`);
